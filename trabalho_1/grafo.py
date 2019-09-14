267250f3d383cf04afafa27e11c9e328dc329c47
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-09-14 12:14:48 -0300

    grafo pronto para usar nos exercícios

--- trabalho_1/grafo.py
+++ trabalho_1/grafo.py
@@ -11,7 +11,7 @@ class Grafo:
             linha = i.split()
             if (linha[0] == '*vertices'):
                 for j in range(int(linha[1])):
-                    indice, rotulo = a.readline().split(' ', 1)
+                    _ , rotulo = a.readline().split(' ', 1)
                     self.vertices_.append([])
                     self.rotulos_.append(rotulo.rstrip())
             if (linha[0] == '*edges'):
@@ -28,19 +28,25 @@ class Grafo:
     	return self.n_arestas_
     
     def grau(self, v):
-    	return len(self.vertices_[v])
+    	return len(self.vertices_[v - 1])
     
     def rotulo(self, v):
-    	return self.rotulos_[v]
+    	return self.rotulos_[v - 1]
     
     def vizinhos(self, v):
-    	return self.vertices_[v]
+    	return self.vertices_[v - 1]
     
     def ha_aresta(self, u, v):
-        return True if u.ah_aresta(v) > (-1) else False
+        for i in self.vertices_[u]:
+            if (v - 1) == i[0]:
+                return True
+        return False
     
     def peso(self, u, v):
-        return u.ah_aresta(v) if u.ah_aresta(v) > (-1) else sys.maxsize
+        for i in self.vertices_[u]:
+            if (v - 1) == i[0]:
+                return i[1]
+        return sys.maxsize
 
 
 grafo = Grafo('facebook_santiago.net')
