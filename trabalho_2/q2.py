3dc977c910c6d357b970595283bc890bd1d773d8
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-10-22 14:45:56 -0300

    topologico imprimindo correto

--- trabalho_2/q2.py
+++ trabalho_2/q2.py
@@ -4,5 +4,7 @@ from structures.ordenacao_topologica import ordenacao_topologica
 grafo = Grafo('grafo.net')
 ordenacao_topologica(grafo)
 
-# for u in range (len(grafo.vertices_)):
-    # print(u)
+string = ""
+for u in range (len(grafo.vertices_)):
+	string += " -> " + grafo.rotulo(u)
+print((string.replace("\n", "")).replace(" -> ", "", 1))
--- trabalho_2/structures/dfs_visit_ot.py
+++ trabalho_2/structures/dfs_visit_ot.py
@@ -2,8 +2,8 @@ def dfs_visit_ot (grafo, v, C, T, F, tempo, lista):
     C[v] = True
     tempo += 1
     T[v] = tempo
-    for u in grafo.vizinhos(v):
-        if (not C[u[0]]):
+    for u in range (len(grafo.vizinhos(v))):
+        if (not C[u]):
             dfs_visit_ot(grafo, u, C, T, F, tempo, lista)
     tempo += 1
     F[v] = tempo
