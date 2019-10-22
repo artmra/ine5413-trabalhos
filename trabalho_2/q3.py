b3c295996e04e54c3eaa248b925e462aec26b5ea
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-10-21 21:26:09 -0300

    algoritmo de Prim pronto, agra fazer o que as questões pedem

--- trabalho_2/q3.py
+++ trabalho_2/q3.py
@@ -1,1 +1,8 @@
-		
+from structures.grafo import Grafo
+from structures.prim import prim_
+
+grafo = Grafo('facebook_santiago.net')
+
+print("Quantidade de vertices : ",grafo.qtd_vertices())
+print("Quantidade de arestas", grafo.qtd_arestas())
+prim_(grafo)		
--- trabalho_2/structures/prim.py
+++ trabalho_2/structures/prim.py
@@ -1,17 +1,24 @@
 import copy
 import sys
-def prim(grafo):
-	r = 0
+def prim_(grafo):
+	r = 10
 	A = dict([(i, None) for i,v in enumerate(grafo.vertices_)])
 	K = dict([(i, sys.maxsize) for i,v in enumerate(grafo.vertices_)])
 	K[r] = 0
-	Q = [A.keys(), K]
-	while not Q[0]:
+	Q = [[i for i in range(len(grafo.vertices_))], K]
+	# print(Q[0])
+	while Q[0]:
 		Q[0].sort(key = lambda u : K[u], reverse = False)
 		u = Q[0].pop(0)
+		# print('u da vez = ', u)
 
-		for v in grafo.vizinhanca(u):
-			if Q[0].count(v) and (grafo.peso(u, v) < K[v]):
+		for v, peso in grafo.vizinhos(u):
+			# print(v, peso)
+			if Q[0].count(v) and (peso < K[v]):
 				A[v] = u
-				K[v] = grafo.peso(u, v)
-	return A
+				K[v] = peso
+			# if v == 396 or v == 627:
+			# 	print (grafo.vertices_[u])
+			# 	print (v, '\n')
+	# print(A.values())
+	return
