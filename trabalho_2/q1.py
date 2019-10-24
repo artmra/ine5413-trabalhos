3595068264f0e7b71b4b92f3e1041e46787e444d
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-10-24 09:02:14 -0300

    cfc corrigido

--- trabalho_2/q1.py
+++ trabalho_2/q1.py
@@ -1,7 +1,7 @@
 from structures.grafo import Grafo
 from structures.componentes_fortemente_conexos import cfc
 
-grafo1 = Grafo('facebook_santiago_dirigido.net')
+grafo1 = Grafo('GrafoCFC.net')
 # link do grafo usado: https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/strong-comps.html
-print('O programa irá imprimir o rótulo dos vértices de cada cfc \n\n')
+print('O programa irá imprimir o rótulo dos vértices de cada cfc')
 cfc(grafo1)
--- trabalho_2/structures/depth_first_search_visit.py
+++ trabalho_2/structures/depth_first_search_visit.py
@@ -3,12 +3,12 @@ def dfs_visit(grafo, v, C, T, A, F, tempo, cfc, adaptado):
     tempo = tempo + 1
     T[v] = tempo
     if adaptado and cfc.count(grafo.vertices_[v][0]) < 1:
-        cfc.append(grafo.vertices_[v][0])
+        cfc.append(grafo.rotulo(v))
 
     for u, peso in grafo.vizinhos_saintes(v):
         if (not C[u]):
             if adaptado and cfc.count(grafo.vertices_[u][0]) < 1:
-                cfc.append(grafo.vertices_[u][0])
+                cfc.append(grafo.rotulo(u))
                  # print(v, '->', u)
             A[u] = v
             tempo, cfc = dfs_visit(grafo, u, C, T, A, F, tempo, cfc, adaptado)
